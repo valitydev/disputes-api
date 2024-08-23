0f65fdb1b77e564839045833bacc96145adfe579
Anatoly Karlov <karleowne@gmail.com>
2024-08-24 00:22:43 +0700

    IMP-268: add mdc async

---
+++ src/main/java/dev/vality/disputes/config/AsyncMDCConfiguration.java
@@ -0,0 +1,21 @@
+package dev.vality.disputes.config;
+
+import dev.vality.disputes.MDCTaskDecorator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@SuppressWarnings("AbbreviationAsWordInName")
+public class AsyncMDCConfiguration {
+
+    @Bean
+    public Executor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setTaskDecorator(new MDCTaskDecorator());
+        executor.initialize();
+        return executor;
+    }
+}
