fc40268fb63136bc402b804090bf38ddd94c5188
Anatoly Karlov <karleowne@gmail.com>
2024-08-25 21:49:28 +0700

    IMP-268: Conditionally Enable Scheduled Jobs

--- src/main/java/dev/vality/disputes/security/AccessService.java
+++ src/main/java/dev/vality/disputes/security/AccessService.java
@@ -33,10 +33,12 @@ public class AccessService {
     }
 
     private AccessData buildAccessData(String invoiceId, String paymentId) {
+        // http 500
         var invoice = invoicingService.getInvoice(invoiceId);
         return AccessData.builder()
                 .invoice(invoice)
                 .payment(getInvoicePayment(invoice, paymentId))
+                // http 500
                 .authData(tokenKeeperService.getAuthData())
                 .build();
     }
@@ -44,10 +46,12 @@ public class AccessService {
     private void checkUserAccessData(AccessData accessData) {
         log.info("Check the user's rights to perform dispute operation");
         try {
+            // http 500
             var resolution = bouncerService.getResolution(accessData);
             switch (resolution.getSetField()) {
                 case FORBIDDEN: {
                     if (authEnabled) {
+                        // http 500
                         throw new AuthorizationException("No rights to perform dispute");
                     } else {
                         log.warn("No rights to perform dispute operation, but auth is disabled");
@@ -62,6 +66,7 @@ public class AccessService {
                                     .filter(shop ->
                                             shop.getId().equals(accessData.getInvoice().getInvoice().getShopId()))
                                     .findFirst()
+                                    // http 500
                                     .orElseThrow(() -> new AuthorizationException("No rights to perform dispute"));
                         }
                     } else {
@@ -72,10 +77,12 @@ public class AccessService {
                 case ALLOWED:
                     break;
                 default:
+                    // http 500
                     throw new BouncerException(String.format("Resolution %s cannot be processed", resolution));
             }
         } catch (Exception e) {
             if (authEnabled) {
+                // http 500
                 throw e;
             }
             log.warn("Auth error occurred, but bouncer check is disabled: ", e);
@@ -88,6 +95,7 @@ public class AccessService {
                 .filter(invoicePayment -> paymentId.equals(invoicePayment.getPayment().getId())
                         && invoicePayment.isSetRoute())
                 .findFirst()
+                // http 404
                 .orElseThrow(() -> new NotFoundException(
                         String.format("Payment with id: %s and filled route not found!", paymentId)));
     }
