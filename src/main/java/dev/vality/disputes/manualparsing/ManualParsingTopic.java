7f052ac5743b7a9c33aafd27a54e32df84213dc6
Anatoly Karlov <karleowne@gmail.com>
2024-08-29 21:46:36 +0700

    add ManualParsing module

--- src/main/java/dev/vality/disputes/manualparsing/ManualParsingTopic.java
+++ src/main/java/dev/vality/disputes/manualparsing/ManualParsingTopic.java
@@ -29,9 +29,11 @@ public class ManualParsingTopic {
 
     @Transactional(propagation = Propagation.REQUIRED)
     void sendDispute(Dispute dispute, List<Attachment> attachments) {
-        log.warn("!! Dispute is needed in Manual Parsing {} {}", dispute, attachments);
+        log.info("!! Dispute is needed in Manual Parsing {} {}", dispute, attachments);
         var contextMap = MDC.getCopyOfContextMap();
         contextMap.put("dispute_id", dispute.getId().toString());
+        contextMap.put("@severity", "warn");
+        contextMap.put("message", String.format("!! Dispute is needed in Manual Parsing %s %s", dispute, attachments));
         MDC.setContextMap(contextMap);
         MDC.clear(); //?? будет ли это работать и откатит ли лог при откате транзакции
     }
