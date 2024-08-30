d9160d66ad8d729c1e6519273091c24fb7725361
Anatoly Karlov <karleowne@gmail.com>
2024-08-30 19:31:18 +0700

    add CreateAdjustment schedulator

--- src/main/resources/db/migration/V2__add_dispute_statuses.sql
+++ src/main/resources/db/migration/V2__add_dispute_statuses.sql
@@ -1,6 +1,4 @@
-alter type dspt.dispute_status add value 'cancelled';
-alter type dspt.dispute_status add value 'manual_created';
-alter type dspt.dispute_status add value 'manual_pending';
-alter type dspt.dispute_status add value 'create_adjustment';
-alter table dspt.dispute
-    add column skip_call_hg_for_create_adjustment boolean not null default true;
+ALTER TYPE dspt.dispute_status ADD VALUE 'cancelled';
+ALTER TYPE dspt.dispute_status ADD VALUE 'manual_created';
+ALTER TYPE dspt.dispute_status ADD VALUE 'manual_pending';
+ALTER TYPE dspt.dispute_status ADD VALUE 'create_adjustment';
