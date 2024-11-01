958fd3eedfa26a03603cd9e953f47317c4fa4950
Anatoly Karlov <karleowne@gmail.com>
2024-11-01 19:36:44 +0700

    add ProviderCallbackDao

--- src/main/resources/db/migration/V5__create_provider_callback_table.sql
+++ src/main/resources/db/migration/V5__create_provider_callback_table.sql
@@ -6,5 +6,5 @@ CREATE TABLE dspt.provider_callback
     status                             dspt.dispute_status         NOT NULL DEFAULT 'create_adjustment' :: dspt.dispute_status,
     changed_amount                     BIGINT,
     skip_call_hg_for_create_adjustment BOOLEAN                     NOT NULL DEFAULT TRUE,
-    CONSTRAINT test_adjusment_from_callback_pkey PRIMARY KEY (id)
+    CONSTRAINT provider_callback_pkey PRIMARY KEY (id)
 );
