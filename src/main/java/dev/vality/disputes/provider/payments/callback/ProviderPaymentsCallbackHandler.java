ed8674aaf3f12b422987516426824208a5f3f642
Anatoly Karlov <karleowne@gmail.com>
2024-12-13 17:42:48 +0700

    use async for handle callback

--- src/main/java/dev/vality/disputes/provider/payments/callback/ProviderPaymentsCallbackHandler.java
+++ src/main/java/dev/vality/disputes/provider/payments/callback/ProviderPaymentsCallbackHandler.java
@@ -1,13 +1,6 @@
 package dev.vality.disputes.provider.payments.callback;
 
-import dev.vality.damsel.domain.TransactionInfo;
-import dev.vality.damsel.payment_processing.InvoicePayment;
-import dev.vality.disputes.exception.NotFoundException;
-import dev.vality.disputes.provider.payments.converter.TransactionContextConverter;
 import dev.vality.disputes.provider.payments.service.ProviderPaymentsService;
-import dev.vality.disputes.schedule.service.ProviderDataService;
-import dev.vality.disputes.service.external.InvoicingService;
-import dev.vality.disputes.utils.PaymentStatusValidator;
 import dev.vality.provider.payments.ProviderPaymentsCallbackParams;
 import dev.vality.provider.payments.ProviderPaymentsCallbackServiceSrv;
 import lombok.RequiredArgsConstructor;
@@ -15,9 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.thrift.TException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -25,16 +15,12 @@ import java.util.Optional;
 @SuppressWarnings({"LineLength"})
 public class ProviderPaymentsCallbackHandler implements ProviderPaymentsCallbackServiceSrv.Iface {
 
-    private final InvoicingService invoicingService;
     private final ProviderPaymentsService providerPaymentsService;
-    private final ProviderDataService providerDataService;
-    private final TransactionContextConverter transactionContextConverter;
 
     @Value("${provider.payments.isProviderCallbackEnabled}")
     private boolean isProviderCallbackEnabled;
 
     @Override
-    @Transactional
     public void createAdjustmentWhenFailedPaymentSuccess(ProviderPaymentsCallbackParams callback) throws TException {
         log.info("Got providerPaymentsCallbackParams {}", callback);
         if (!isProviderCallbackEnabled) {
@@ -44,31 +30,6 @@ public class ProviderPaymentsCallbackHandler implements ProviderPaymentsCallback
             log.info("InvoiceId should be set, finish");
             return;
         }
-        try {
-            var invoiceId = callback.getInvoiceId().get();
-            var paymentId = callback.getPaymentId().get();
-            var invoicePayment = invoicingService.getInvoicePayment(invoiceId, paymentId);
-            log.debug("Got invoicePayment {}", callback);
-            // validate
-            PaymentStatusValidator.checkStatus(invoicePayment);
-            // validate
-            var providerTrxId = getProviderTrxId(invoicePayment);
-            var providerData = providerDataService.getProviderData(invoicePayment);
-            var transactionContext = transactionContextConverter.convert(invoiceId, paymentId, providerTrxId, providerData);
-            var currency = providerDataService.getCurrency(invoicePayment);
-            var invoiceAmount = invoicePayment.getPayment().getCost().getAmount();
-            providerPaymentsService.checkPaymentStatusAndSave(transactionContext, currency, providerData, invoiceAmount);
-        } catch (NotFoundException ex) {
-            log.warn("NotFound when handle ProviderPaymentsCallbackParams, type={}", ex.getType(), ex);
-        } catch (Throwable ex) {
-            log.warn("Failed to handle ProviderPaymentsCallbackParams", ex);
-        }
-    }
-
-    private String getProviderTrxId(InvoicePayment payment) {
-        return Optional.ofNullable(payment.getLastTransactionInfo())
-                .map(TransactionInfo::getId)
-                .orElseThrow(() -> new NotFoundException(
-                        String.format("Payment with id: %s and filled ProviderTrxId not found!", payment.getPayment().getId()), NotFoundException.Type.PROVIDERTRXID));
+        providerPaymentsService.processCallback(callback);
     }
 }
