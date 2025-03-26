8279f03ae72363a4034b0063c878e2932c98ede8
Anatoly Karlov <karleowne@gmail.com>
2025-03-26 19:15:45 +0700

    rm design forgotten scheduler

--- src/main/java/dev/vality/disputes/schedule/ForgottenDisputesTask.java
+++ src/main/java/dev/vality/disputes/schedule/ForgottenDisputesTask.java
@@ -1,7 +1,5 @@
 package dev.vality.disputes.schedule;
 
-import dev.vality.disputes.admin.callback.CallbackNotifier;
-import dev.vality.disputes.admin.management.MdcTopicProducer;
 import dev.vality.disputes.domain.tables.pojos.Dispute;
 import dev.vality.disputes.schedule.core.ForgottenDisputesService;
 import dev.vality.disputes.schedule.handler.ForgottenDisputeHandler;
@@ -25,20 +23,15 @@ import java.util.stream.Collectors;
 public class ForgottenDisputesTask {
 
     private final ForgottenDisputesService forgottenDisputesService;
-    private final CallbackNotifier callbackNotifier;
-    private final MdcTopicProducer mdcTopicProducer;
     private final ExecutorService disputesThreadPool;
 
     @Value("${dispute.batchSize}")
     private int batchSize;
 
-    @Value("${dispute.isCronForgottenDisputesNotificationsEnabled}")
-    private boolean isCronForgottenDisputesNotificationsEnabled;
-
     @Scheduled(fixedDelayString = "${dispute.fixedDelayForgotten}", initialDelayString = "${dispute.initialDelayForgotten}")
     public void processForgottenDisputes() {
-        var disputes = forgottenDisputesService.getForgottenSkipLocked(batchSize);
         try {
+            var disputes = forgottenDisputesService.getForgottenSkipLocked(batchSize);
             var callables = disputes.stream()
                     .map(this::handleForgotten)
                     .collect(Collectors.toList());
@@ -48,17 +41,6 @@ public class ForgottenDisputesTask {
             Thread.currentThread().interrupt();
         } catch (Throwable ex) {
             log.error("Received exception while scheduler processed Forgotten disputes", ex);
-        }
-    }
-
-    @Scheduled(cron = "${dispute.cronForgottenDisputesNotifications:-}")
-    public void cronForgottenDisputes() {
-        if (isCronForgottenDisputesNotificationsEnabled) {
-            var disputes = forgottenDisputesService.getForgottenSkipLocked(Integer.MAX_VALUE);
-            if (!disputes.isEmpty()) {
-                callbackNotifier.sendForgottenDisputes(disputes);
-                mdcTopicProducer.sendForgottenDisputes(disputes);
-            }
         }
     }
 
