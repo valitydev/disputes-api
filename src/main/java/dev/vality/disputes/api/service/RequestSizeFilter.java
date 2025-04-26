2255ac98290f454ce96c6ac2f44334b45d12f4a7
Anatoly Karlov <karleowne@gmail.com>
2025-04-26 22:10:04 +0700

    add tests

--- src/main/java/dev/vality/disputes/api/service/RequestSizeFilter.java
+++ src/main/java/dev/vality/disputes/api/service/RequestSizeFilter.java
@@ -8,17 +8,21 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class RequestSizeFilter extends OncePerRequestFilter {
 
-    private static final int MAX_REQUEST_SIZE = 10 * 1024 * 1024; // 10 MB
+    @Value("${dispute.maxRequestSize}")
+    private long maxRequestSize;
 
     private final ObjectMapper customObjectMapper;
 
@@ -27,7 +31,8 @@ public class RequestSizeFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) {
-        if (request.getContentLengthLong() > MAX_REQUEST_SIZE) {
+        if (request.getContentLengthLong() > maxRequestSize * 1024 * 1024) {
+            log.warn("413 SC_REQUEST_ENTITY_TOO_LARGE: invalid request {}", getRequestInfo(request));
             response.sendError(HttpServletResponse.SC_REQUEST_ENTITY_TOO_LARGE, getMessage());
             return;
         }
@@ -37,5 +42,25 @@ public class RequestSizeFilter extends OncePerRequestFilter {
     private String getMessage() throws JsonProcessingException {
         return customObjectMapper.writeValueAsString(new GeneralError()
                 .message("Blocked: Request too large"));
+    }
+
+    private String getRequestInfo(HttpServletRequest request) {
+        var info = new StringBuilder();
+        info.append("method=").append(request.getMethod())
+                .append(", url=").append(request.getRequestURL())
+                .append(", uri=").append(request.getRequestURI())
+                .append(", query=").append(request.getQueryString())
+                .append(", contentLength=").append(request.getContentLengthLong())
+                .append(", contentType=").append(request.getContentType());
+
+        info.append(", headers=[");
+        var headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            info.append(headerName).append("=").append(request.getHeader(headerName)).append("; ");
+        }
+        info.append("]");
+
+        return info.toString();
     }
 }
